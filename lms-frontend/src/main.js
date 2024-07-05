47aeb52f6136e5a77b0766898f96f82ad3801d05
Shijuchi <shanedelatorre2412@gmail.com>
2024-07-06 01:00:03 +0800

    removed unnecessary files and setup tailwind and bulma

--- lms-frontend/src/main.js
+++ lms-frontend/src/main.js
@@ -3,12 +3,16 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import axios from "axios";
+
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+axios.defaults.baseURL = 'http://127.0.0.1:8000/api/v1/'
+
 app.use(createPinia())
-app.use(router)
+app.use(router,axios)
 
 app.mount('#app')
