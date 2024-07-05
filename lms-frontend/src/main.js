5c513ee785ceeda0d6965318e60c89eb2d9d5e16
Shijuchi <shanedelatorre2412@gmail.com>
2024-07-05 23:44:58 +0800

    reinstall frontend using vite

--- lms-frontend/src/main.js
+++ lms-frontend/src/main.js
@@ -1,9 +1,14 @@
+import './assets/main.css'
+
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
 import App from './App.vue'
 import router from './router'
-import store from './store'
-import axios from 'axios'
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000'
+const app = createApp(App)
 
-createApp(App).use(store).use(router, axios).mount('#app')
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
