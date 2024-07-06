70ba106f8433036437ef76b1c41d451eedf1dcc7
Shijuchi <shanedelatorre2412@gmail.com>
2024-07-07 01:12:00 +0800

    refractored some folder structure, also added new routes

--- lms-frontend/src/router/index.js
+++ lms-frontend/src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LandingPageView from "@/views/LandingPageView.vue";
+import LandingPageView from "@/views/landing_page/LandingPageView.vue";
+import AboutView from "@/views/landing_page/AboutView.vue";
+import SignupView from "@/views/landing_page/SignupView.vue";
+import LoginView from "@/views/landing_page/LoginView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +11,21 @@ const router = createRouter({
       path: '/',
       name: 'landing-page',
       component: LandingPageView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: AboutView
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: SignupView
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView
     },
   ]
 })
