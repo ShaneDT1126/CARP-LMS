4614f712eae789993e79991b912ec42d42140418
Shijuchi <shanedelatorre2412@gmail.com>
2024-07-07 01:01:49 +0800

    made a folder structure and file structure

--- lms-frontend/src/App.vue
+++ lms-frontend/src/App.vue
@@ -3,11 +3,7 @@
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-    <br>
-  </h1>
-  <button class="button">Button</button>
+  <router-view/>
 </template>
 
 <style lang="scss">
--- lms-frontend/src/router/index.js
+++ lms-frontend/src/router/index.js
@@ -1,10 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import LandingPageView from "@/views/LandingPageView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-
+    {
+      path: '/',
+      name: 'landing-page',
+      component: LandingPageView
+    },
   ]
 })
 
