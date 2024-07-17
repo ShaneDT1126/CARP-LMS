e98fce1f60c57a7da223a576eaaa9e23f3cdb56a
Shijuchi <shanedelatorre2412@gmail.com>
2024-07-18 00:49:07 +0800

    added new components in students

---
+++ lms-frontend/src/components/student/StudentClassesComponent.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "StudentClassesComponent"
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ lms-frontend/src/components/student/StudentDashboardComponent.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "StudentDashboardComponent"
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
