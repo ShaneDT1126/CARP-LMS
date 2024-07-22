0cabaf2c75b282e75d0527e144f469ea451c567e
Shijuchi <shanedelatorre2412@gmail.com>
2024-07-22 23:00:18 +0800

    Refactor the Student Dashboard nav to its own component

---
+++ lms-frontend/src/components/common/NavbarComponent.vue
@@ -0,0 +1,65 @@
+<template>
+  <nav class="w-full lg:w-56 bg-blue-600 text-white p-3 flex flex-col justify-between">
+    <div>
+      <div class="mb-8">
+        <h6 class="text-xl font-bold">CARP.ED</h6>
+      </div>
+      <ul class="mt-8 lg:mt-24 space-y-4">
+        <li>
+          <router-link to="" class="block mb-5 pl-2 hover:bg-blue-700 rounded-md text-white outline outline-offset-2 outline-1">Dashboard</router-link>
+        </li>
+        <li>
+          <router-link to="" class="block mb-5 pl-2 hover:bg-blue-700 rounded-md text-white outline outline-offset-2 outline-1">Classes</router-link>
+        </li>
+        <li>
+          <router-link to="" class="block pl-2 hover:bg-blue-700 rounded-md text-white outline outline-offset-2 outline-1">CARP</router-link>
+        </li>
+      </ul>
+    </div>
+    <div class="mt-auto">
+      <button @click="logoutModalToggle" class="block pl-2 underline underline-offset-2 text-white">Logout</button>
+      <Dialog v-model:visible="visible" modal header="Are you sure you want to logout?" :style="{ width: '25rem' }">
+        <button @click="logout">Yes</button>
+        <button @click="logoutModalToggle">No</button>
+      </Dialog>
+    </div>
+  </nav>
+</template>
+
+
+<script>
+import Dialog  from "primevue/dialog";
+import {useUserStore} from "@/stores/userStore.js";
+
+export default {
+  name: 'NavbarComponent',
+
+  components: {
+   Dialog,
+  },
+
+  setup(){
+    const userStore = useUserStore()
+
+    return {
+      userStore
+    }
+  },
+
+  data(){
+    return {
+      visible: false
+    }
+  },
+
+  methods:{
+    logoutModalToggle(){
+      this.visible = !this.visible
+    },
+    logout(){
+      this.userStore.removeToken()
+      this.$router.push({name: 'login-page'})
+    },
+  }
+}
+</script>
--- lms-frontend/src/views/student/StudentDashboardView.vue
+++ lms-frontend/src/views/student/StudentDashboardView.vue
@@ -1,31 +1,7 @@
 <template>
   <div class="flex flex-col lg:flex-row h-screen bg-gray-100">
     <!-- Sidebar -->
-    <nav class="w-full lg:w-56 bg-blue-600 text-white p-3 flex flex-col justify-between">
-      <div>
-        <div class="mb-8">
-          <h6 class="text-xl font-bold">CARP.ED</h6>
-        </div>
-        <ul class="mt-8 lg:mt-24 space-y-4">
-          <li>
-            <router-link to="" class="block mb-5 pl-2 hover:bg-blue-700 rounded-md text-white outline outline-offset-2 outline-1">Dashboard</router-link>
-          </li>
-          <li>
-            <router-link to="" class="block mb-5 pl-2 hover:bg-blue-700 rounded-md text-white outline outline-offset-2 outline-1">Classes</router-link>
-          </li>
-          <li>
-            <router-link to="" class="block pl-2 hover:bg-blue-700 rounded-md text-white outline outline-offset-2 outline-1">CARP</router-link>
-          </li>
-        </ul>
-      </div>
-      <div class="mt-auto">
-        <button @click="logoutModalToggle" class="block pl-2 underline underline-offset-2 text-white">Logout</button>
-        <Dialog v-model:visible="visible" modal header="Are you sure you want to logout?" :style="{ width: '25rem' }">
-          <button @click="logout">Yes</button>
-          <button @click="logoutModalToggle">No</button>
-        </Dialog>
-      </div>
-    </nav>
+    <NavbarComponent />
 
     <!-- Main Content -->
     <div class="flex-1 flex flex-col overflow-hidden">
@@ -77,12 +53,14 @@
 <script>
 import Dialog from "primevue/dialog";
 import {useUserStore} from "@/stores/userStore.js";
+import NavbarComponent from "@/components/common/NavbarComponent.vue";
 
 export default {
   name: 'StudentDashboardView',
 
   components: {
-    Dialog
+    Dialog,
+    NavbarComponent
   },
 
   setup(){
@@ -106,13 +84,7 @@ export default {
   },
 
   methods:{
-    logoutModalToggle(){
-      this.visible = !this.visible
-    },
-    logout(){
-      this.userStore.removeToken()
-      this.$router.push({name: 'login-page'})
-    },
+
   },
 
   mounted() {
